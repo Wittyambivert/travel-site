3e57bdf84f6d92f01e8c1cc2021d94e43178d6d9
Max <wittyambivert.com>
2020-09-22 14:33:51 -0800

    completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -29,8 +29,10 @@ jane.payTaxes();
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
+import StickyHeader from "./modules/StickyHeader";
 
 let mobileMenu = new MobileMenu();
 // let revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+let stickyHeader = new StickyHeader();
